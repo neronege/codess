c196ee53f29c1cb9d9f51989527283235fa59ab6
Egemen Ozmen <72514229+neronege@users.noreply.github.com>
2024-03-12 03:35:47 +0300

    Add files via upload

--- java/com/exam/taskNew/service/GitLabApiService.java
+++ java/com/exam/taskNew/service/GitLabApiService.java
@@ -3,10 +3,13 @@ package com.exam.taskNew.service;
 import java.util.List;
 
 import com.exam.taskNew.model.Commit;
+import com.exam.taskNew.model.Developer;
 
 
 public interface GitLabApiService{
-	List<Commit> getCommits(String username, String repositoryName, String sinceDate);
-	List<Commit> getAllCommits(String username, String repositoryName); 
-	Commit getCommitById(Long id);
+	List<Commit> getCommits(String projectId);
+	List<Developer> getDevelopers(String projectId);
+	List<Commit>GetCommitDetail(String projectId);
+	Commit getCommitById(Long Id);
+
 }
--- java/com/exam/taskNew/service/GitLapApiServiceImpl.java
+++ java/com/exam/taskNew/service/GitLapApiServiceImpl.java
@@ -1,10 +1,10 @@
 package com.exam.taskNew.service;
 
+import java.util.ArrayList;
 
-
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -14,75 +14,174 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.exam.taskNew.model.Commit;
+import com.exam.taskNew.model.*;
+
 
 import com.exam.taskNew.repository.CommitRepository;
+import com.exam.taskNew.repository.DeveloperRepository;
+import com.exam.taskNew.model.DTO.GitLabCommit;
 
 
 @Service
-public class GitLapApiServiceImpl implements GitLabApiService {
-    private final String GITHUB_API_BASE_URL = "https://api.github.com";
+public class GitLapApiServiceImpl implements GitLabApiService{
+	private final String gitLabApiBaseUrl = "https://gitlab.example.com/api/v4";
     private final RestTemplate restTemplate;
     private final CommitRepository commitRepository;
-
-   
+    private final DeveloperRepository developerRepository;
+    private final String accessToken = "Bearer glpat-c-H3imEf6QUsGhjs9Bv1";
     
-    
-    public GitLapApiServiceImpl(RestTemplate restTemplate,CommitRepository commitRepository) {
+    public GitLapApiServiceImpl(RestTemplate restTemplate, DeveloperRepository developerRepository,
+    		CommitRepository commitRepository) {
         this.restTemplate = restTemplate;
 		this.commitRepository = commitRepository;
-		
+        this.developerRepository = developerRepository;
     }
 
+  
     @Override
-    public List<Commit> getCommits(String username, String repositoryName, String sinceDate)
-    {
-    	HttpHeaders headers = new HttpHeaders();
-	 //   headers.setContentType(MediaType.APPLICATION_JSON);
-	    headers.set("Authorization", "Bearer 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");
-	   // HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-        String apiUrl = String.format("%s/repos/%s/%s/commits?since=2023-02-01T00:00:00Z", GITHUB_API_BASE_URL, username, repositoryName, sinceDate);
-        
-       // ResponseEntity<Commit[]> response = restTemplate.getForEntity(apiUrl, Commit[].class);
+    public List<Commit>getCommits(String projectId) {
+      
+        // URL oluşturma
+        String apiUrl = String.format("%s/projects/%s/repository/commits", gitLabApiBaseUrl, projectId);
 
-			HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
-			
-			ResponseEntity<String> response = restTemplate.exchange(
-			apiUrl, HttpMethod.GET, requestEntity, String.class);
+        // HTTP başlıklarını ayarlama
+        HttpHeaders headers = new HttpHeaders();
+        //headers.set("Authorization", "Bearer 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");
+        headers.set("Authorization", accessToken);
+        // HTTP isteği oluşturma
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+
+        // REST çağrısı
+        ResponseEntity<GitLabCommit[]> response = restTemplate.exchange(
+                apiUrl, HttpMethod.GET, requestEntity, GitLabCommit[].class);
+
+        // API yanıtını işleme
         if (response.getStatusCode() == HttpStatus.OK) {
-            System.out.println(response.getBody());
-            return Collections.emptyList();
+            GitLabCommit[] gitLabCommits = response.getBody();
+
+            if (gitLabCommits != null && gitLabCommits.length > 0) {
+                List<Commit> commits = new ArrayList<>();
+
+                for (GitLabCommit gitLabCommit : gitLabCommits) {
+                    Commit commit = new Commit();
+                    commit.setMessage(gitLabCommit.getMessage());
+                    ;
+                    commit.setAuthor(gitLabCommit.getAuthorName());
+                    commit.setTimestamp(gitLabCommit.getCommittedDate());
+
+                    commits.add(commit);
+                }
+
+                // Burada commits listesini kaydetme işlemini istediğiniz şekilde gerçekleştirebilirsiniz
+
+                return commits;
+            } else {
+                return Collections.emptyList();
+            }
         } else {
-            // Handle error
             return Collections.emptyList();
         }
-     
     }
-    @Override
-    public List<Commit> getAllCommits(String username, String repositoryName) 
-    {
-        String apiUrl = String.format("%s/repos/%s/%s/commits", GITHUB_API_BASE_URL, username, repositoryName);
 
-        ResponseEntity<Commit[]> response = restTemplate.getForEntity(apiUrl, Commit[].class);
 
+	@SuppressWarnings("unlikely-arg-type")
+	@Override
+	public List<Developer>getDevelopers(String projectId) {
+		 // URL oluşturma
+        String apiUrl = String.format("%s/projects/%s/repository/commits", gitLabApiBaseUrl, projectId);
+
+        // HTTP başlıklarını ayarlama
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", accessToken);
+
+        // HTTP isteği oluşturma
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+
+        // REST çağrısı
+        ResponseEntity<GitLabCommit[]> response = restTemplate.exchange(
+                apiUrl, HttpMethod.GET, requestEntity, GitLabCommit[].class);
+
+        // API yanıtını işleme
         if (response.getStatusCode() == HttpStatus.OK) {
-            List<Commit> commits = Arrays.asList(response.getBody());
+            GitLabCommit[] gitLabCommits = response.getBody();
 
-            // GitHub'dan alınan commit verilerini veritabanına kaydet
-            commitRepository.saveAll(commits);
-
-            return commits;
+            if (gitLabCommits != null && gitLabCommits.length > 0) {
+	   			
+	   			    List<Developer> developers = new ArrayList<>();
+	   			 
+	   			    for (GitLabCommit gitLabCommit : gitLabCommits) {
+	   			    	Developer developer = new Developer();
+	   			    	developer.setUsername(gitLabCommit.getAuthorName());
+	   			    	developer.setEmail(gitLabCommit.getAuthorEmail());
+	   			    	developer.setMessage(gitLabCommit.getMessage());
+	   			      if(!developers.contains(developer.hashCode())) {
+		   	            developers.add(developer);
+	   			      }
+	   			    }
+	   			   developerRepository.saveAll(developers);
+	   		
+	               return developers;
+            } else {
+                return Collections.emptyList();
+            }
         } else {
-            // Handle error
+            return Collections.emptyList();
+        }
+    }
+
+
+
+	@Override
+	public List<Commit> GetCommitDetail(String projectId) {
+		// URL oluşturma
+        String apiUrl = String.format("%s/projects/%s/repository/commits/main", gitLabApiBaseUrl, projectId);
+
+        // HTTP başlıklarını ayarlama
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", accessToken);
+
+        // HTTP isteği oluşturma
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+
+        // REST çağrısı
+        ResponseEntity<GitLabCommit[]> response = restTemplate.exchange(
+                apiUrl, HttpMethod.GET, requestEntity, GitLabCommit[].class);
+
+        // API yanıtını işleme
+        if (response.getStatusCode() == HttpStatus.OK) {
+            GitLabCommit[] gitLabCommits = response.getBody();
+
+            if (gitLabCommits != null && gitLabCommits.length > 0) {
+                List<Commit> commits = new ArrayList<>();
+
+                for (GitLabCommit gitLabCommit : gitLabCommits) {
+                    Commit commit = new Commit();
+                    commit.setMessage(gitLabCommit.getMessage());
+                    commit.setHash(gitLabCommit.getAuthor().getSha());
+                    commit.setAuthor(gitLabCommit.getAuthorName());
+                    commit.setTimestamp(gitLabCommit.getCommittedDate());
+                  
+                    commits.add(commit);
+                }
+
+                // Burada commits listesini kaydetme işlemini istediğiniz şekilde gerçekleştirebilirsiniz
+
+                return commits;
+            } else {
+                return Collections.emptyList();
+            }
+        } else {
             return Collections.emptyList();
         }
     }
 
 	@Override
 	public Commit getCommitById(Long id) {
-		java.util.Optional<Commit> optionalCommit = this.commitRepository.findById(id);
-        return optionalCommit.orElse(null);
+		// TODO Auto-generated method stub
+		return null;
 	}
+ }
 
-	
-}
+
+
+
